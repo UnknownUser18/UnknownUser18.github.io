cc5786d9c90abb3de2c1df2536696e6a9bbdc543
UnknownUser18 <unknownuser75@proton.me>
2025-05-07 10:41:53 +0200

    added hamburger and gallery

---
+++ assets/scripts/burger-menu.js
@@ -0,0 +1,40 @@
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('nav');
+const data = {
+  'Strona Główna': '/',
+  'Produkty': 'products/',
+  'Blog': 'blog/',
+  'Kontakt': 'contact/',
+  'Historia': 'historia/'
+};
+hamburger.addEventListener('click', () => {
+  document.body.classList.toggle('no-scroll');
+  if(window.scrollY === 0)
+    nav.classList.toggle('black');
+  hamburger.classList.toggle('active');
+  if(hamburger.classList.contains('active')) {
+    let menu = document.createElement('ul');
+    for (let name of Object.keys(data)) {
+      let li = document.createElement('li');
+      let a = document.createElement('a');
+      console.log(window.location.pathname)
+      if ((data[name] && window.location.pathname === `/${data[name]}`) || (!data[name] && window.location.pathname === '/'))
+        li.classList.add('active');
+      a.href = data[name];
+      a.title = data[name];
+      a.innerText = name;
+      li.appendChild(a);
+      menu.appendChild(li);
+    }
+    document.body.insertBefore(menu, document.querySelector('main'));
+    setTimeout(() => {
+     menu.classList.add('done');
+    }, 200);
+  } else {
+    let menu = document.querySelector('body > ul');
+    menu.classList.remove('done');
+    setTimeout(() => {
+      menu.remove();
+    }, 200);
+  }
+});
