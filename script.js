b30a156b9b092da26beb03ff5261b5acaa4dd576
UnknownUser18 <unknownuser75@proton.me>
2025-06-09 19:39:29 +0200

    Finished HTML and SCSS

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ const scrollable = document.querySelector('.scrollable');
 const thumb = document.querySelector('.scroller-thumb');
 const figures = document.querySelectorAll('#debugging article > figure');
 const articles = document.querySelectorAll('.scrollable article');
-
+const nav = document.querySelector('nav');
+let navWasShown = false;
+let lastScrollY = window.scrollY;
 const h1 = document.querySelector('h1');
 const caret = document.createElement('span');
 caret.className = 'caret';
@@ -83,64 +85,49 @@ function updateAll() {
   updateThumb();
   updateFiguresVisibility();
   updateArticlesVisibility();
+}
+
+function updateAllWindow() {
+  updateThumb();
+  updateFiguresVisibility();
+  updateArticlesVisibility();
   checkNavSpan();
+  checkIfSectionVisible();
 }
 
 scrollable.addEventListener('scroll', updateAll);
-window.addEventListener('resize', updateAll);
-window.addEventListener('scroll', checkNavSpan);
-updateAll();
+window.addEventListener('resize', updateAllWindow);
+window.addEventListener('scroll', updateAllWindow);
 
 function createChart() {
   const ctx = document.getElementById('wykres').getContext('2d');
   const chart = new Chart(ctx, {
-    type : 'line',
-    data : {
-      labels : [ '1', '2', '3', '4', '5', '6', '7', '8', '9', '10' ],
-      datasets : [ {
-        label : 'Grupa kontrolna',
-        data : [ 10, 11, 12, 13, 14, 15, 16, 17, 18, 20 ],
-        borderColor : '#babbf1',
-        fill : false,
-        tension : 0.1
+    type : 'line', data : {
+      labels : [ '1', '2', '3', '4', '5', '6', '7', '8', '9', '10' ], datasets : [ {
+        label : 'Grupa kontrolna', data : [ 10, 11, 12, 13, 14, 15, 16, 17, 18, 20 ], borderColor : '#babbf1', fill : false, tension : 0.1
       }, {
-        label : 'Rubber Duck Debugging',
-        data : [ 10, 12, 14, 16, 18, 20, 22, 25, 28, 30 ],
-        borderColor : '#e5c890',
-        fill : false,
-        tension : 0.1
+        label : 'Rubber Duck Debugging', data : [ 10, 12, 14, 16, 18, 20, 22, 25, 28, 30 ], borderColor : '#e5c890', fill : false, tension : 0.1
       } ]
-    },
-    options : {
+    }, options : {
       animation : {
-        duration : 1500,
-        easing : 'easeInOutQuad'
-      },
-      plugins : {
+        duration : 1500, easing : 'easeInOutQuad'
+      }, plugins : {
         legend : {
           labels : {
             color : '#babbf1'
           }
         }
-      },
-      scales : {
+      }, scales : {
         x : {
           title : {
-            display : true,
-            text : 'Dzień ',
-            color : '#babbf1'
-          },
-          ticks : {
+            display : true, text : 'Dzień ', color : '#babbf1'
+          }, ticks : {
             color : '#babbf1'
           }
-        },
-        y : {
+        }, y : {
           title : {
-            display : true,
-            text : 'Liczba błędów',
-            color : '#babbf1'
-          },
-          ticks : {
+            display : true, text : 'Liczba błędów', color : '#babbf1'
+          }, ticks : {
             color : '#babbf1'
           }
         }
@@ -154,9 +141,9 @@ function checkNavSpan() {
     if (isElementVisible(section) && (index === 0 || !isElementVisible(sections[index - 1]))) {
       const navElement = document.querySelector(`nav li:nth-child(${ index + 1 })`);
       let left = parseInt(getComputedStyle(navElement).width);
-      if (index === 1) left += 10;
-      else if (index > 1) left += 20 * (index - 1);
-      else left = 0;
+      if (index === 0) left = 0;
+      else if (index === 1) left += 10;
+      else left += 30 + ((index - 1) * left);
       span.style.left = `${ left }px`;
       let color;
       switch (index) {
@@ -166,18 +153,52 @@ function checkNavSpan() {
         case 1:
           color = 'var(--green)';
           break;
+        case 2:
+          color = 'var(--pink)';
+          break;
       }
       span.style.outlineColor = color;
     }
   });
 }
 
-
 function isElementVisible(el) {
   const rect = el.getBoundingClientRect();
-  return (
-    rect.bottom > 0 &&
-    rect.top < window.innerHeight / 2 &&
-    rect.bottom > window.innerHeight / 2
-  );
+  return (rect.bottom > 0 && rect.top < window.innerHeight / 2 && rect.bottom > window.innerHeight / 2);
 }
+
+
+function checkIfSectionVisible() {
+  const section = document.querySelector('section:first-of-type');
+  const rect = section.getBoundingClientRect();
+  const scrolledDown = window.scrollY > lastScrollY;
+  lastScrollY = window.scrollY;
+
+  const halfVisible = rect.top < window.innerHeight / 2 && rect.bottom > window.innerHeight / 2;
+
+  if (!navWasShown && halfVisible) {
+    nav.classList.add('visible');
+    navWasShown = true;
+  }
+  else if (!navWasShown && !scrolledDown && rect.bottom <= 0) {
+    nav.classList.remove('visible');
+    navWasShown = false;
+  }
+}
+
+const form = document.querySelector('form');
+const indexes = ['yellow', 'red', 'orange'];
+form.addEventListener('input', () => {
+  const input = form.querySelector('input:checked');
+  const value = input.value.trim();
+  const articles = document.querySelectorAll('section:nth-of-type(2) article');
+  articles.forEach((article, index) => {
+    article.classList.toggle('hidden', index !== indexes.indexOf(value));
+  });
+});
+
+const img = document.querySelector('footer img');
+img.addEventListener('click', () => {
+  const audio = document.querySelector('audio');
+  audio.play().then();
+})
